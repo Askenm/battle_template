0ffd6af5be364053d20f717b829b76039b749953
Aske Meineche <askemeineche@Askes-MacBook-Pro.local>
2024-01-25 16:39:08 +0100

    testing DB write 2

--- test.py
+++ test.py
@@ -12,6 +12,6 @@ battle_data = {
             '_TOURNAMENT_ID_':'29',
             '_BATTLE_REPO_': 'vinder.com',
             '_BATTLE_CREATOR_': '42',
-            '_END_DATE_': '2024-23-04'
+            '_END_DATE_': '2024-04-23'
         }
 DBMS_.write('CREATE_BATTLE',battle_data)
